ceb5a5e261b3e91449b5156ac7f145d1b9c966df
kapitsa2811 <46664776+kapitsa2811@users.noreply.github.com>
2021-11-04 20:35:06 +0530

    Update main_line_ctc.py

--- OCR/line_OCR/ctc/main_line_ctc.py
+++ OCR/line_OCR/ctc/main_line_ctc.py
@@ -65,7 +65,7 @@ def train_and_test(rank, params):
 
 
 if __name__ == "__main__":
-    dataset_name = "IAM"  # ["RIMES", "IAM", "READ_2016"]
+    dataset_name = "READ_2016" #"IAM"  # ["RIMES", "IAM", "READ_2016"]
 
     params = {
         "dataset_params": {
